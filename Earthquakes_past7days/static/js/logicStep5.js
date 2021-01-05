1d03482a31090c69c9f9dd2b3e165e36c54d5aea
su-linterhell <sulinterhell@gmail.com>
2021-01-04 19:11:21 -0800

    tweaking code

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -105,6 +105,10 @@ d3.json(earthquakes_7days).then(function(data) {
 
   }).addTo(earthquakes);
 
+  // Then we add the earthquake layer to our map 
+  earthquakes.addTo(map);
+
+
   // Create a legend control object.
   let legend = L.control({
     position: "bottomright"
@@ -113,27 +117,28 @@ d3.json(earthquakes_7days).then(function(data) {
   // Then add all the details for the legend.
   legend.onAdd = function() {
     let div = L.DomUtil.create("div", "info legend");
-      const magnitudes = [0, 1, 2, 3, 4, 5];
-      const colors = [
+      
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
         "#98ee00",
         "#d4ee00",
         "#eecc00",
         "#ee9c00",
         "#ea822c",
         "#ea2c2c"
-      ];
+    ];
     // Looping through our intervals to generate a label with a colored square for each interval.
     for (var i = 0; i < magnitudes.length; i++) {
       console.log(colors[i]);
       div.innerHTML +=
         "<i style='background: " + colors[i] + "'></i> " +
         magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
-  }
+    }
     return div;
   };
 
+  // Add legend to map 
   legend.addTo(map);
 
-    // Then we add the earthquake layer to our map 
-    earthquakes.addTo(map);
-    });
+});
+    
